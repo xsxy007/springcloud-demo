9320a63842d50e9ad6363ab6b36d91a9d5f7f0aa
nxz_zhe@163.com <nxz_zhe@163.com>
2019-11-19 15:06:32 +0800

    基础代码修改

--- eureka-consumer/src/main/java/com/xsxy/eurekaconsumer/controller/EurekaConsumerController.java
+++ eureka-consumer/src/main/java/com/xsxy/eurekaconsumer/controller/EurekaConsumerController.java
@@ -1,16 +1,10 @@
 package com.xsxy.eurekaconsumer.controller;
 
-import com.netflix.appinfo.ApplicationInfoManager;
-import com.netflix.discovery.StatusChangeEvent;
-import com.netflix.discovery.TimedSupervisorTask;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import sun.plugin2.main.server.HeartbeatThread;
-
-import java.util.concurrent.TimeUnit;
 
 @RestController
 public class EurekaConsumerController {
